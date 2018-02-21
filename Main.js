2fb8c0a5b0520743d49c69468de439d975d2e9f2
slikkistvan@gmail.com <slikkistvan@gmail.com>
2018-02-21 21:11:34 +0100

    upgrade-mechanics-done

--- Main.js
+++ Main.js
@@ -4,11 +4,9 @@ let pixels=[];
 let size=20;
 let score;
 let mouse=new Mouse();
-let speed=200;
-let maxLevel=4;
-let basePoint=1;
-let test=6;
+
 let counter=0;
+
 
 $('#garea').ready(function Start(){
 	area= new Area(560,480);
@@ -16,21 +14,33 @@ $('#garea').ready(function Start(){
 	mouse.start();
 	walkers[0]=new Walker(280,240);
 	score= new Score();
-	$('#menubutton4').css('background-color','#1aff53');
-	$('#menu4').css('display','block');
-
+	$('#menubutton2').css({"background-color":"#1aff53",
+			"color":"#3d5c5c"});
+	$('#menu2').css('display','block');
+	for (let i=0;i<3;i++){
+		printLevel(i);
+	}
 	//createDebugTools();
 	
 })
 
 function Update(){
+	
 	area.update();
-	//walker.walk();
-	if(counter==test){
-	walkers.forEach(element=>element.walk());
-	counter=0;		
-	}else{counter++;}
+	
+
+	
+
+	if(counter==speed){
+		walkers.forEach(element=>element.walk());
+		counter=0;}
+	
+	if (counter>speed){counter=0;}
+	
+	counter++;
+	
 	pixels.forEach(element=>element.update());
+
 	mouse.index=getIndexFromPosition(mouse.pos);
 	hover();
 
--- Upgrades.js
+++ Upgrades.js
@@ -1,3 +1,18 @@
+let prices=[
+	[200,500,750,1150,1500,2000],
+	[150,400,600,900,1500],
+	[120,300,500,800,1000]
+]
+//color
+//speed
+//value
+
+let levels=[1,1,1];
+
+let maxLevel=4*levels[0];
+let speed=6/levels[1];
+let basePoint=1*levels[2];
+
 $(function(){
 	$("#menuholder li").click(function(){
 		let id=$(this).attr('id');
@@ -5,8 +20,49 @@ $(function(){
 		id=id.substr("menubutton".length,1);
 		$(".menus").css("display","none");
 		$("#menu"+id).css("display","block");
-		$("#menuholder li").css("background-color","#367d36")
+		$("#menuholder li").css({"background-color":"#2e6b2e",
+			"color":"#c2d6d6"})
 				
-		$("#menubutton"+id).css("background-color","#1aff53")
+		$("#menubutton"+id).css({"background-color":"#1aff53",
+			"color":"#3d5c5c"})
 	})
 })
+
+function canAfford(bValue){
+	//score<=prices[bValue][levels[bValue]] ?  true :  false;
+	if(score.point>=prices[bValue][levels[bValue]-1]){return true;}
+	else{return false;}
+}
+
+$(function(){
+	$(".bupgradeb").click(function(){
+		let id=$(this).val();
+		console.log(score);
+		if(canAfford(id)){
+			score.update(-prices[id][levels[id]-1]);
+			levels[id]++;
+			maxLevel=4*levels[0];
+			speed=Math.floor(6/levels[1]);
+			if(id==1){counter=0;}
+			basePoint=1*levels[2];
+			$("#price"+id).html("<b>"+prices[id][levels[id]]+"</b>");
+			printLevel(id);
+		}
+		console.log(levels);
+	})
+})
+
+function printLevel(id){
+	$("#levels"+id).html("");
+	let n=prices[id].length
+	for (let i=n;i>0;i--){
+		if(i>levels[id]){
+			
+		$("#levels"+id).append('<div style="margin-top:2px; height:10px; width:100%; background-color:white;"></div>');
+		}else{
+		$("#levels"+id).append('<div style="margin-top:2px; height:10px; width:100%; background-color:black;"></div>');
+
+		}
+	}
+}
+
